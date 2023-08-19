257208bb153d93201d42637ab8224c68567b313c
Adam TG <adam@gawelczyk.com>
2023-08-20 00:25:33 +0200

    switching to MassTransit.Grpc

--- sdk1/Program.cs
+++ sdk1/Program.cs
@@ -8,7 +8,17 @@ var builder = WebApplication.CreateBuilder();
 builder.Services.AddMassTransit(x =>
 {
     x.AddConsumers(typeof(Program).Assembly);
-    x.UsingInMemory((context, cfg) => { cfg.ConfigureEndpoints(context); });
+
+    //x.UsingInMemory((context, cfg) => { cfg.ConfigureEndpoints(context); });
+    x.UsingGrpc((context, cfg) =>
+    {
+        cfg.Host(h =>
+        {
+            h.Host = "127.0.0.1";
+            h.Port = 19796;
+        });
+        cfg.ConfigureEndpoints(context);
+    });
 });
 
 
